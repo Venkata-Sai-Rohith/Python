77fee81390800418e8faf387714b8b1e50e0784e
Venkata-Sai-Rohith <108536321+Venkata-Sai-Rohith@users.noreply.github.com>
2022-08-16 14:50:21 +0530

    Add files via upload

---
+++ booleanExercise.py
@@ -0,0 +1,11 @@
+#To compare the age of two persons
+myage = 26
+userage = int(input("Enter your age: ") )
+
+if myage < userage:
+    print("user is older than you: ",userage)
+elif myage > userage:
+    print("user is younger than you: ",userage)
+else:
+    print("you both are of same age: ",userage)
+    
